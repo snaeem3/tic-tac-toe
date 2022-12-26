d744b7a6d84669beebd35d17c69393cd1820e6db
Sameer <sameer4363@gmail.com>
2022-12-26 14:50:11 -0500

    Add eslint files

--- script.js
+++ script.js
@@ -1,75 +1,86 @@
 const gameWrapper = document.querySelector('#game-wrapper');
 
 const gameBoard = (() => {
-    let board = [
-        ['','',''],
-        ['','',''],
-        ['','','']
-    ];
+  const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
 
-    const makeMove = (col,row,symbol) => {
-        if (!checkEmptyPosition(col,row)) {
-            console.error('position is not empty');
-            return;
-        }
-
-        board[col][row] = symbol;
-        console.table(board);
-        return checkVictory();
+  const makeMove = (col, row, symbol) => {
+    if (!checkEmptyPosition(col, row)) {
+      console.error('position is not empty');
+      return;
     }
 
-    const checkEmptyPosition = (col,row) => {
-        return board[col][row] === '';
+    board[col][row] = symbol;
+    console.table(board);
+    return checkVictory();
+  };
+
+  const checkEmptyPosition = (col, row) => board[col][row] === '';
+
+  const checkVictory = () => {
+    // Checks gameBoard for 3 identical symbols and returns the symbol that won
+
+    // Check each column
+    for (let c = 0; c < 3; c++) {
+      if (
+        checkEmptyPosition(c, 0) ||
+        checkEmptyPosition(c, 1) ||
+        checkEmptyPosition(c, 2)
+      ) {
+        continue;
+      }
+
+      if (board[c][0] === board[c][1] && board[c][0] === board[c][2]) {
+        return board[c][0];
+      }
+    }
+    // Check each row
+    for (let r = 0; r < 3; r++) {
+      if (
+        checkEmptyPosition(0, r) ||
+        checkEmptyPosition(1, r) ||
+        checkEmptyPosition(2, r)
+      ) {
+        continue;
+      }
+
+      if (board[0][r] === board[1][r] && board[0][r] === board[2][r]) {
+        return board[0][r];
+      }
     }
 
-    const checkVictory = () => {
-        // Checks gameBoard for 3 identical symbols and returns the symbol that won
-
-        // Check each column
-        for(let c = 0; c < 3; c++) {
-            if (checkEmptyPosition(c,0) || checkEmptyPosition(c,1) || checkEmptyPosition (c,2)) {
-                continue;
-            }
-
-            if (board[c][0] === board[c][1] && board[c][0] === board[c][2]) {
-                return board[c][0];
-            }
-        }
-        // Check each row
-        for(let r = 0; r < 3; r++) {
-            if (checkEmptyPosition(0,r) || checkEmptyPosition(1,r) || checkEmptyPosition (2,r)) {
-                continue;
-            }
-
-            if (board[0][r] === board[1][r] && board[0][r] === board[2][r]) {
-                return board[0][r];
-            }
-        }
-
-        // Check each diagonal
-        // Negative diagonal
-        if (!checkEmptyPosition(0,0) && !checkEmptyPosition(1,1) && checkEmptyPosition(2,2)) {
-            if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-                return board[1][1];
-            }
-        }
-        // Positive diagonal
-        if (!checkEmptyPosition(0,2) && !checkEmptyPosition(1,1) && checkEmptyPosition(2,0)) {
-            if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
-                return board[1][1];
-            }
-        }
-
-        // Return empty string if no winner
-        return '';
+    // Check negative diagonal
+    if (
+      !checkEmptyPosition(0, 0) &&
+      !checkEmptyPosition(1, 1) &&
+      checkEmptyPosition(2, 2)
+    ) {
+      if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+        return board[1][1];
+      }
     }
-    
-    return {
-        board,
-        makeMove,
-    };
+    // Check positive diagonal
+    if (
+      !checkEmptyPosition(0, 2) &&
+      !checkEmptyPosition(1, 1) &&
+      checkEmptyPosition(2, 0)
+    ) {
+      if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+        return board[1][1];
+      }
+    }
+
+    // Return empty string if no winner
+    return '';
+  };
+
+  return {
+    board,
+    makeMove,
+  };
 })();
 
-const player = (symbol) => {
-    return {symbol};
-}
+const player = (symbol) => ({ symbol });
